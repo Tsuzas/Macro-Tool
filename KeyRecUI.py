4a4be25dd45cc8286f9bd846fbc2e19218341983
Tsuzas <f.pereira.2002@hotmail.com>
2026-04-29 12:04:33 +0100

    fix(KeyRecUi): removed useless while and flag
    docs(KeyRecUi): improvement of inline comments

--- KeyRecUI.py
+++ KeyRecUI.py
@@ -10,9 +10,6 @@ from tkinter import simpledialog, messagebox
 # LISTENER VARIABLE FLAG
 listenerTrigger = False
 
-# LISTENER FOR OPTIONS TO KEEP RUNNING
-optionsFlag = True
-
 #CHANGES THE JSON PATH TO THE CURRENT FOLDER
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
@@ -20,12 +17,16 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 settings_path = os.path.join(BASE_DIR, "settings.json")
 sentences_path = os.path.join(BASE_DIR, "sentences.json")
 
-# Quick function to clear Input field where user types
+# ================ CLEAR INPUT FUNCTION ================= #
+#                   Clears userOption                     #
+#           so it doesn't keep the previous option        #
 def clearInput():
     global userOption
     userOption.delete(0, tk.END)
 
-# Saves macro by appending on array then pushing to Json
+# ================ SAVE MACRO FUNCTION ================== #
+#          Saves macro by appending to array              #
+#                  And pushing to Json                    #
 def saveMacro(event=None):
     global sentenceList, userOption
     if (userOption.get() == ""):
@@ -37,10 +38,12 @@ def saveMacro(event=None):
             json.dump(sentenceList, f)
         messagebox.showinfo("Saved", f"Macro saved: {macro}")
         clearInput()
-    
+
     goBackMenu()
 
-# Shows All Macros, edits from input then pushes to array, and Json
+# ================ EDIT MACRO FUNCTION ================== #
+#           Shows all macros, edits from input            #
+#            then pushes to array and to Json             #
 def editMacro():
     global sentenceList
     infoText = "\n".join(f"[{i}] --> {word}" for i, word in enumerate(sentenceList))
@@ -60,8 +63,8 @@ def editMacro():
     else:
         messagebox.showinfo("Error", "Invalid index.")
 
-# === LOAD SETTINGS ===
-#     # VIA jSon #
+# === LOAD SETTINGS === #
+#     # VIA jSon #     #
 def loadSettings():
     try:
         with open(settings_path, "r") as f:
@@ -74,7 +77,8 @@ def loadSettings():
             "loops": 1
         }
 
-# Setting Macro, defines time for input, input speed, and repeats?
+# ================ SAVE SETTINGS FUNCTION ================== #
+#       Defines time for input, input speed, and loops       #
 def saveSettings():
     global settingList, delayOption, macroSpeedOption, loopOption, delayWindow, macroSpeed, loop, delayWindow
 
@@ -135,7 +139,8 @@ def saveSettings():
     
     goBackMenu()
 
-# Resets macro by pushing an empty list to Json
+# ================ CLEAR MACRO LIST FUNCTION ================== #
+#         Resets macro by pushing an empty list to Json         #
 def resetMacros():
     global sentenceList
     
@@ -155,6 +160,10 @@ def resetMacros():
     
     goBackMenu()
 
+# ================ PLACEHOLDERS FOR SETTINGS ================== #
+#        Whole shenanigans, putting values in field             #
+#        Then tunning colour until user clicks on it            #
+#        Clearing the input field allowing user to write        #
 def entryFocusIn(event):
     widget = event.widget
 
@@ -170,6 +179,7 @@ def entryFocusIn(event):
         widget.delete(0, 'end')
         widget.config(fg="Black")
 
+# ================ CREATES SETTING WINDOWS  ================== #
 def settingsWindow():
     global delayWindow, delayOption,macroSpeed, macroSpeedOption, loop, loopOption, resetMacrosButton, sentenceList
     
@@ -237,108 +247,112 @@ def settingsWindow():
     # ================================================= #
 
 
-# Options of app with their subsequent code
+# ================ OPTION MENU FOR USER ================== #
+#                  Switch case with all                    #
+#             The options related code on it               #
 def optionSelect(event=None):
     ## LIST OF OPTION  = [1, 2 ,3 ,4 ,8 ,9]
     
-    while optionsFlag == True:
-        global userOption, mainLabel, selectButton, sentenceList, topFrame
+    global userOption, mainLabel, selectButton, sentenceList, topFrame
 
 
-        # TRY CATCH 
-        # FLOATS, STRINGS AND EMPTY VALUES
-        try:
-            option = int(userOption.get())
-        except ValueError:
-            messagebox.showerror("Error", "Please enter a valid number.")
-            return
+    # ===================== TRY CATCH BLOCK ===================== #
+    try:
+        option = int(userOption.get())
+    except ValueError:
+        messagebox.showerror("Error", "Please enter a valid number.")
+        return
+    # ============ Catches String Floats and Empty ============== #
 
-        match option:
-            # Gets Sentence from Input then uses function SaveMacro()
-            case 1:
-                clearInput()
-                mainLabel.config(text="Write the sentence you wish to add below:\n\nEmpty input will count as aborted")
-                selectButton.config(text="Add Macro", command=saveMacro)
+    match option:
+        # ==================== SWITCH CASE ====================== #
+        # Gets Sentence from Input then uses function SaveMacro() #
 
-            # Shows all Sentences from Array on a Quick Window
-            case 2:
-                clearInput()
-                if not sentenceList:
-                    messagebox.showinfo("Macros", "No macros saved yet.")
-                else:
-                    infoText = "\n".join(f"[{i}] --> {word}" for i, word in enumerate(sentenceList))
-                    messagebox.showinfo("Macro List", infoText)
-            case 3:
-                # If no macros, quick display alerting User, if there's Sentences editMacro()
-                clearInput()
-                if not sentenceList:
-                    messagebox.showinfo("Macros", "No macros to edit.")
-                else:
-                    editMacro()
-            case 4:
-                clearInput()
-                if not sentenceList:
-                    messagebox.showinfo("Macros", "No macros saved yet.")
-                    return
-            
-                # Lets User choose which macro to replicate
+        case 1:
+            clearInput()
+            mainLabel.config(text="Write the sentence you wish to add below:\n\nEmpty input will count as aborted")
+            selectButton.config(text="Add Macro", command=saveMacro)
+
+        # Shows all Sentences from Array on a Quick Window
+        case 2:
+            clearInput()
+            if not sentenceList:
+                messagebox.showinfo("Macros", "No macros saved yet.")
+            else:
                 infoText = "\n".join(f"[{i}] --> {word}" for i, word in enumerate(sentenceList))
-                macroEdit = simpledialog.askinteger("Macro List", f"Choose the macro to execute:\n{infoText}")
+                messagebox.showinfo("Macro List", infoText)
+        case 3:
+            # If no macros, quick display alerting User, if there's Sentences editMacro()
+            clearInput()
+            if not sentenceList:
+                messagebox.showinfo("Macros", "No macros to edit.")
+            else:
+                editMacro()
+        case 4:
+            clearInput()
+            if not sentenceList:
+                messagebox.showinfo("Macros", "No macros saved yet.")
+                return
+        
+            # Lets User choose which macro to replicate
+            infoText = "\n".join(f"[{i}] --> {word}" for i, word in enumerate(sentenceList))
+            macroEdit = simpledialog.askinteger("Macro List", f"Choose the macro to execute:\n{infoText}")
 
-                # Simple error handling
-                if macroEdit is None or not (0 <= macroEdit < len(sentenceList)):
-                    messagebox.showinfo("Cancelled", "Invalid selection.")
-                    return
+            # Simple error handling
+            if macroEdit is None or not (0 <= macroEdit < len(sentenceList)):
+                messagebox.showinfo("Cancelled", "Invalid selection.")
+                return
 
-                macroWord = sentenceList[macroEdit]
+            macroWord = sentenceList[macroEdit]
 
-                #ASKS FOR LISTENER
-                permission = allowListener()
-                if permission == 1:
-                    while listenerTrigger != True:
-                        time.sleep(0.1)
+            #ASKS FOR LISTENER
+            permission = allowListener()
+            if permission == 1:
+                while listenerTrigger != True:
+                    time.sleep(0.1)
 
-                    # What types for you
-                    kb = Controller()
-                    loops = int(settingList["loops"])  # convert to int para rodar no FOR
-                    for _ in range(loops):
-                        for letter in macroWord:
-                            print(letter)
-                            kb.tap(letter)
-                            time.sleep(settingList["macro_speed"])
-                        kb.tap(Key.enter)
-                    messagebox.showinfo("Success", "The macro was executed successfully.")
-                    listener.stop()
-                else:
-                    # Quick warning, alerting user to, after clicking ok, swithc to wanted window.
-                    messagebox.showinfo(
-                        "Macro Starting...",
-                        f'-- {macroWord} -- will start typing in {settingList["delay_speed"]} seconds after closing this window.\nPrepare the target window!'
-                    )
-                    time.sleep(settingList["delay_speed"])
-                    # What types for you
-                    kb = Controller()
-                    loops = int(settingList["loops"])  # convert to int para rodar no FOR
-                    for _ in range(loops):
-                        for letter in macroWord:
-                            kb.tap(letter)
-                            time.sleep(settingList["macro_speed"])
-                        kb.tap(Key.enter)
-                    messagebox.showinfo("Success", "The macro was executed successfully.")
-                    
-            # Settings
-            case 8:
-                settingsWindow()
-            # Exits app
-            case 9:
-                sys.exit()
-            
-            case _:
-                messagebox.showerror("Warning", "Noone of the options were selected")
-                clearInput()
+                # What types for you
+                kb = Controller()
+                loops = int(settingList["loops"])  # convert to int para rodar no FOR
+                for _ in range(loops):
+                    for letter in macroWord:
+                        print(letter)
+                        kb.tap(letter)
+                        time.sleep(settingList["macro_speed"])
+                    kb.tap(Key.enter)
+                messagebox.showinfo("Success", "The macro was executed successfully.")
+                listener.stop()
+            else:
+                # Quick warning, alerting user to, after clicking ok, swithc to wanted window.
+                messagebox.showinfo(
+                    "Macro Starting...",
+                    f'-- {macroWord} -- will start typing in {settingList["delay_speed"]} seconds after closing this window.\nPrepare the target window!'
+                )
+                time.sleep(settingList["delay_speed"])
+                # What types for you
+                kb = Controller()
+                loops = int(settingList["loops"])  # convert to int para rodar no FOR
+                for _ in range(loops):
+                    for letter in macroWord:
+                        kb.tap(letter)
+                        time.sleep(settingList["macro_speed"])
+                    kb.tap(Key.enter)
+                messagebox.showinfo("Success", "The macro was executed successfully.")
+
+        # Settings
+        case 8:
+            settingsWindow()
+        # Exits app
+        case 9:
+            sys.exit()
+        # Wild card for numbers not in previous switches
+        case _:
+            messagebox.showerror("Warning", "Noone of the options were selected")
+            clearInput()
 
 
-# App's Main Menu and UI
+# ================ MAIN MENU ================== #
+#           Creates the first menu              #
 def mainMenu():
     global root, userOption, mainLabel, selectButton, sentenceList, topFrame, topFrame2, topFrame3, settingList
     
@@ -382,7 +396,9 @@ def mainMenu():
     userOption.bind("<Return>", lambda event: selectButton.invoke())
     root.mainloop()
 
-# Called to go back to Main Menu and prevent various windows to be opened
+# ================ GO BACK MENU ================== #
+#              Goes back to Main menu              #
+#           Also prevent multiple windows          #
 def goBackMenu(): 
     clearInput()
 
@@ -406,6 +422,7 @@ def goBackMenu():
     # Re assigns the proper command on the button.
     selectButton.config(text="Choose", command=optionSelect)
 
+# ================ LISTENER ================== #
 def on_press(key):
         global listenerTrigger
         if key == keyboard.Key.esc:
@@ -421,7 +438,10 @@ def on_press(key):
             # self.keys.append(k)  # store it in global-like variable
             print('Key pressed: ' + k)
             return False  # stop listener; remove this if want more keys 
-           
+
+# ========== LISTENER PERMISSIONS ============ #
+#               Quick user prompt              #
+#    To allow listener before macro starts     #
 def allowListener():
     global userOption, listener
     response = messagebox.askquestion("Allow Listener", "Would you like it to listen for a hotkey press (default F10) to initiate or start right now?")
@@ -437,5 +457,5 @@ def allowListener():
     else: return
 
 
-# Start the App
+# ========== APLICATION YUPI ============ #
 mainMenu()
